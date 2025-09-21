21991bc9e15a4e43f00105a6a725b182c5a64f70
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 00:57:00 +0700

    belajar string

---
+++ string.py
@@ -0,0 +1,12 @@
+#Belajar String
+
+nama = "Rully Ar Rafii"
+kota = 'Bandung'
+alamat = """Jl. Kebon Jeruk No. 27"""
+
+nama_depan = "Rully"
+nama_belakang = "Ar Rafii"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)
+print(alamat)
