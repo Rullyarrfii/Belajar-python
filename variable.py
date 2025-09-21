d52d92d23bcfb1d4ddbd681a16393439d1ed83db
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 00:53:10 +0700

    belajar variabel

---
+++ variable.py
@@ -0,0 +1,27 @@
+hello = "Selamat Datang"
+
+#menampilkan variabel
+print(hello)
+
+print(hello)
+
+print(hello)
+
+hello = "Hello Python"
+
+#menampilkan variabel berubah
+
+print(hello)
+
+print(hello)
+
+# menampilkan langsung
+print("Halo Dunia")
+
+print("Halo Dunia")
+
+print("Halo Dunia")
+
+print("Halo Dunia")
+
+print("Halo Dunia")
