5ef9063c43b88b75e9a1146030237399980dee9d
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 17:55:58 +0700

    belajar membuat operator perbandingan

---
+++ operator_perbandingan.py
@@ -0,0 +1,15 @@
+#operator perbandingan
+# lebih dari (>)
+# kurang dari (<)
+# lebih dari sama dengan (>=)
+# kurang dari sama dengan (<=)
+# sama dengan (==) //bisa untuk mengecek tipe data string
+# tidak sama dengan (!=) //bisa untuk mengecek tipe data string
+# hasilnya boolean (true/false)
+
+print(1<=1)
+print(10 == 10)
+print(5 != 2)
+
+print("andi" == "andi")
+print("andi" != "andi")
