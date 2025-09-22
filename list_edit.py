29cc18f02a03133ea5bbb1b8ea4d5aa057acb138
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 18:36:41 +0700

    belajar mengedit isi dalam list

---
+++ list_edit.py
@@ -0,0 +1,16 @@
+#membuat list
+nama = ["Andi", "Budi", "Caca"]
+
+#Menambahkan data ke dalam list
+nama.append("Dodi")
+
+#mengakses data dalam list
+nama[0]
+
+#Menghapus data dalam list
+nama.remove("Budi")
+
+# Mengubah data dalam list
+print(nama)
+nama[0] = "Andi Setiawan"
+print(nama)
