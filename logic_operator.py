38d117db283a76113012096307017dc73ab91811
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 17:49:54 +0700

    belajar membuat logic operator

---
+++ logic_operator.py
@@ -0,0 +1,13 @@
+#Operator Logika
+
+#AND(and)
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+#OR(or)
+print(True or False)
+print(True or False)
+print(False or True)
+print(False or False)
