f07c1b01be9f0bffa08f1ab0669625e220891dd4
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 18:04:52 +0700

    belajar membuat if

---
+++ if.py
@@ -0,0 +1,16 @@
+#Belajar IF-Statement
+#hasilnya boolean (True/False)
+#jika kondisi terpenuhi, maka jalankan kode di dalamnya
+
+orang = 1000
+zombie = 100
+
+if orang < zombie:
+    print("Kabur, dunia mau kiamat!")
+
+if orang > zombie:
+    print("Ayo kita lawan!")
+
+if orang == zombie:
+    print("Mari bertarung!")
+
