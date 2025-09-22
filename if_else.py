91222118e8670e62a40a4b61651052335dd6e3b3
Rullyarrfii <rully.jr45@gmail.com>
2025-09-22 18:10:50 +0700

    belajar membuat if_else

---
+++ if_else.py
@@ -0,0 +1,18 @@
+#Belajar IF ELSE
+
+#tidak menggunakan else
+menang = False
+
+if menang == True:
+    print("selamat")
+
+if menang == False:
+    print("coba lagi!")
+
+#menggunakan else
+win = False
+
+if win == True:
+    print("Selamat")
+else:
+    print("Coba lagi!  ")
