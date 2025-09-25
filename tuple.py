bba8041e382029ad5d03e5505bdd341cb2c68a29
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 18:08:09 +0700

    belajar tuple

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# belajar tuple
+
+pelanggan = ("eko", "joko", "andi")
+
+# pelanggan[0] = "Hengky"
+
+print(pelanggan)
+print(pelanggan[0])
+print(pelanggan[1])
+print(pelanggan[2])
+
+# list datanya bisa di ubah, jika tuple datanya tidak bisa di ubah/ data immutable 
+# akses datanya sama kaya di list cukup ketik indexnya
+# cocok digunakan untuk methode, untuk mengembalikan data. dimana prosedur untuk mengembalikan data. dimana prosedur mengembalikan data dari sebuah proses lebih dari 1 data
