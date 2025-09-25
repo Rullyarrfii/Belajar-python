3cd7f7b3c047b1dedc9f05452fada04b0e8c14bf
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 18:40:44 +0700

    membuat method_parameter

---
+++ method_parameter.py
@@ -0,0 +1,15 @@
+# belajar method parameter
+
+# def say_hello(name):
+#     print(f"hello {name}")
+
+# say_hello("eko")
+# say_hello("uwi")
+
+def say_hello(first_name, last_name): #menambahkan 2 parameter
+    print(f"hello {first_name} {last_name}")
+
+say_hello("eko","budhi")
+
+# yang dimaksud parameter adalah keterangan yang didalam kurung nama methode. nama_method(nilai/value/data_parameter)
+# tidak hanya 1 parameter, bisa 2 parameter
