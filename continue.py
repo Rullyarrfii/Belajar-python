5ad58a86dad0bf07889b28a0af69ba29b37b4468
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 17:57:56 +0700

    menambahkan continue bisa untuk apa

--- continue.py
+++ continue.py
@@ -9,3 +9,5 @@ for a in range(1,100):
     if a % 2 == 1: # i modulus 2 sama dengan 0, jika modulus bukan 1 maka di lanjutkan/ditulis, angka genap
             continue #menghentikan proses looping, dan melanjutkan ke proses berikutnya
     print(a)
+
+    #kata kunci continue, tidak hanya for loop, tapi bisa juga while
