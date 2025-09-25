e68ec31d1d5333f48bd9e2b6bbe2adf596ef39fe
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 17:51:22 +0700

    menambahkan penjelasan continue

--- continue.py
+++ continue.py
@@ -7,5 +7,5 @@
 
 for a in range(1,100):
     if a % 2 == 1: # i modulus 2 sama dengan 0, jika modulus bukan 1 maka di lanjutkan/ditulis, angka genap
-            continue
+            continue #menghentikan proses looping, dan melanjutkan ke proses berikutnya
     print(a)
