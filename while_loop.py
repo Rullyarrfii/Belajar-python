f3fc0d9bfebca90c1f8b6bd72b6de0b9d5b19ff7
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 17:29:27 +0700

    belajar while loop

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+#Belajar while-loop
+
+data = ""
+
+while data != "x":
+    print("masuk perulangan")
+    data = input("data: ")
+
+    # pengulangan akan terhenti jika kita menuliskan xatau sesuai kondisi
