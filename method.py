3452297b2eae1f78a28807de2eb457e4376a9176
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 18:33:49 +0700

    belajar membuat method

---
+++ method.py
@@ -0,0 +1,22 @@
+# belajar membuat method/function
+
+nama = [] #listnama
+
+def print_nama():
+    print("===================")
+    for data in nama:
+        print(data)
+
+nama.append("eko")
+print_nama()
+
+nama.append("hengky")
+print_nama()
+
+nama.append("uwi")
+print_nama()
+
+# method/fungsi menyimpan kode blok, menggunakan kata kunci def
+# print_nama => nama method
+# memanggil method hanya nama_method()
+# panggil methodnya harus berada dibawah method
