0a0ac28b1574c0666abb88f101f3d0dd9e423557
Rullyarrfii <rully.jr45@gmail.com>
2025-09-25 17:36:21 +0700

    belajar continue

---
+++ continue.py
@@ -0,0 +1,11 @@
+# belajar continue
+
+# for i in range(1,100):
+#     if i % 2 == 0: # i modulus 2 sama dengan 0, jika modulus bukan 0 maka di lanjutkan/ditulis, angka ganjil
+#         continue
+#     print(i)
+
+for a in range(1,100):
+    if a % 2 == 1: # i modulus 2 sama dengan 0, jika modulus bukan 1 maka di lanjutkan/ditulis, angka genap
+            continue
+    print(a)
