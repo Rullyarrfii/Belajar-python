e39696bc791559846f34ac4d224ec69397d1a521
Rullyarrfii <rully.jr45@gmail.com>
2025-10-02 13:10:30 +0700

    Belajar tentang numeric yang ada di python

---
+++ numeric_type.py
@@ -0,0 +1,16 @@
+# Belajar Numeric Types in Python
+
+# 1. Integer - Bilangan Bulat
+# 2. Float - Bilangan Desimal
+# 3. Complex - real + imaginery
+
+# contoh 1 : Float
+# angka = 1.5 # python dengan kalkulator bahasa inggris menggunakan titik (.) sebagai pemisah desimal
+# print(angka)
+
+# contoh 2 : Integer -> Float
+angka = 1
+angka2 = 1.5
+total = angka + angka2
+print(total)
+print(10/3) # pembagian selalu menghasilkan float
